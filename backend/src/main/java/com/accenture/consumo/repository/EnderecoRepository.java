4e3fe3564ec2a4d0fd8096f251b708b8ebd7a74d
Thiago Ribeiro <thiagoribeiroramos_@outlook.com>
2025-01-24 19:37:21 -0300

    Delete backend directory

--- backend/src/main/java/com/accenture/consumo/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.consumo.repository;
-
-import com.accenture.consumo.model.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, String> {
-}
